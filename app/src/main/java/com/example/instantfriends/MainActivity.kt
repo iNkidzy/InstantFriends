b941282a5cf155818c7ef4c06cb77352166dda85
iNKidzy <nadq.nadia@gmail.com>
2021-05-06 01:01:11 +0200

    refactorLayouts+fab button+listview

--- app/src/main/java/com/example/instantfriends/MainActivity.kt
+++ app/src/main/java/com/example/instantfriends/MainActivity.kt
@@ -8,18 +8,25 @@ import androidx.navigation.ui.NavigationUI.setupActionBarWithNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        // Fab button
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with opening new post activity", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show() }
+        //Fab button
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
         val navController = findNavController(R.id.nav_host_fragment)
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(setOf(
-            R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_friends))
+            R.id.navigation_home, R.id.navigation_friends))
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
     }
