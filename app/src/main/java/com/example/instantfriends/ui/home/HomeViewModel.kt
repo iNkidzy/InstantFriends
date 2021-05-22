dbe54ebbc555e239903d4ecb26cc1f3073b4e913
iNKidzy <nadq.nadia@gmail.com>
2021-05-22 22:35:53 +0200

    redesign

--- app/src/main/java/com/example/instantfriends/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/instantfriends/ui/home/HomeViewModel.kt
@@ -1,5 +1,8 @@
 package com.example.instantfriends.ui.home
 
+import android.app.PendingIntent.getActivity
+import android.content.Intent
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -10,7 +13,6 @@ class HomeViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
         value = "This is the PostPage"
     }
-
 
     val text: LiveData<String> = _text
 }
