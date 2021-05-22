d09770af93012a1bb38f9eda3190066fed8cd572
iNKidzy <nadq.nadia@gmail.com>
2021-05-22 23:42:49 +0200

    Refactor

--- app/src/main/java/com/example/instantfriends/ui/friends/FriendsFragment.kt
+++ app/src/main/java/com/example/instantfriends/ui/friends/FriendsFragment.kt
@@ -9,7 +9,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.instantfriends.R
-import com.example.instantfriends.dummy.DummyContent
 
 /**
  * A fragment representing a list of Items.
