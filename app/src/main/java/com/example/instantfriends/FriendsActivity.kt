50ca7ece823302417fe9df2137a6049c1d4bad50
daiperboevs42 <54886017+daiperboevs42@users.noreply.github.com>
2021-05-26 21:37:23 +0200

    FriendsActivity added

---
+++ app/src/main/java/com/example/instantfriends/FriendsActivity.kt
@@ -0,0 +1,56 @@
+package com.example.instantfriends
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import com.example.instantfriends.data.BEFriend
+import com.example.instantfriends.data.FriendRepository
+import com.example.instantfriends.data.observeOnce
+
+class FriendsActivity: AppCompatActivity() {
+    private var cache: List<BEFriend>? = null;
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_friends)
+        setupDataObserver()
+    }
+
+    private fun setupDataObserver() {
+        val lvNames = findViewById<ListView>(R.id.ListView)
+        val mRep = FriendRepository.get()
+        val nameObserver = Observer<List<BEFriend>>{ friends ->
+            cache = friends;
+            val asStrings = friends.map { f -> "${f.id}, ${f.name}"}
+            val adapter: ListAdapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_list_item_1,
+                asStrings.toTypedArray()
+            )
+            lvNames.adapter = adapter
+        }
+        mRep.getAll().observe(this, nameObserver)
+        lvNames.onItemClickListener = AdapterView.OnItemClickListener { _, _, pos, _ -> onClickFriend(pos)}
+    }
+
+    private fun onClickFriend(pos: Int) {
+        val id = cache!![pos].id
+        val friendObserver = Observer<BEFriend> { friend ->
+            if  (friend != null)
+            {
+                val intent = Intent(this, DetailsActivity::class.java).also {
+                    it.putExtra("position", id)
+                    startActivity(it)
+                }
+            }
+        }
+        val mRep = FriendRepository.get()
+        mRep.getByid(id).observeOnce(this, friendObserver)
+    }
+}
