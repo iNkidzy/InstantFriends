0b7dcbb6e8290d01dee375e7803b34552c91ba69
daiperboevs42 <54886017+daiperboevs42@users.noreply.github.com>
2021-05-26 15:26:27 +0200

    createpost activity created and intent added to main

---
+++ app/src/main/java/com/example/instantfriends/CreatePostActivity.kt
@@ -0,0 +1,41 @@
+package com.example.instantfriends
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.instantfriends.data.BEPost
+import com.example.instantfriends.data.PostRepository
+import com.google.android.material.textfield.TextInputEditText
+
+class CreatePostActivity : AppCompatActivity(){
+
+    private var imgPath: String = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.create_post)
+    }
+
+    private fun clickNewPost(view: View){
+        val mRep = PostRepository.get()
+        val postDescription: TextInputEditText = findViewById(R.id.postDescription)
+
+        if(imgPath != "") {
+            val post = BEPost(
+                0,
+                imgPath,
+                postDescription.text.toString()
+            )
+            mRep.insert(post)
+            Toast.makeText(this, "Your post has been created", Toast.LENGTH_SHORT).show()
+            finish()
+        }
+        else{
+            Toast.makeText(this, "Please take a photo first", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+}
